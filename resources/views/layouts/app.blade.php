3d901c8107b7d745efafbb15017258853a0497c8
Kurniawan <kurniawanpputra@gmail.com>
2019-11-07 10:45:39 +0700

    Create landing page, create message feature, fix login register layouts and change application theme color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <span style="letter-spacing: 1px;" class="text-bold">ESRIVA</span>
+                    <span style="letter-spacing: 1px;">ESRIVA</span>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
